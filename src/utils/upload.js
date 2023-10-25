183f40bf9d4b2c2fd989601a08518dd689f4cbab
Angel-trabajo <ovnicrofordz@gmail.com>
2023-10-25 11:14:55 -0300

    entrega final

--- src/utils/upload.js
+++ src/utils/upload.js
@@ -1,33 +1,32 @@
 import multer from "multer";
 import __dirname from "./dirname.js";
 
+
 const storage = multer.diskStorage({
-    destination: function(req, file, cb){
-        /* let destination = `${__dirname}/../public/documents`; */
-        let destination ="";
-        console.log('CONSOLE LOG ' +file);
-        if (file.fieldname === 'profileImage'){
-            destination = `${__dirname}/../public/profiles`;
-        } else if (file.fieldname === "productImage"){
-            destination = `${__dirname}/../public/products`;
-        }else if (file.fieldname === "identification" || "proofOfAddress" || "accountStatus "){
-            destination = `${__dirname}/../public/documents`
+    destination: function (req, file, cb) {
+        let destination = ""; 
+        
+        if (file.fieldname === 'profileImage') {
+            destination = "./src/public/profiles"; 
+        } else if (file.fieldname === "productImage") {
+            destination = "./src/public/products"; 
+        } else if (["identification", "proofOfAddress", "accountStatus"].includes(file.fieldname)) {
+            destination = "./src/public/documents"; 
         }
 
         cb(null, destination);
     },
-    filename: function(req, file, cb){
-        // Agrego un identificador al nombre del archivo basado en el campo del formulario
+    filename: function (req, file, cb) {
         const fieldName = file.fieldname === 'profileImage' ? 'profile' :
-                          file.fieldname === 'productImage' ? 'product' :
-                          file.fieldname === 'identification'? 'identification':
-                          file.fieldname === 'proofOfAddress'? 'proofOfAddress':
-                          'accountStatus';
+            file.fieldname === 'productImage' ? 'product' :
+            file.fieldname === 'identification' ? 'identification' :
+            file.fieldname === 'proofOfAddress' ? 'proofOfAddress' :
+            'accountStatus';
         const filename = `${fieldName}-${Date.now()}-${file.originalname}`;
         cb(null, filename);
-    } //ejemplo : "product-1696299477963-foto..jpg"
+    }
 });
 
-const uploader = multer({storage})
+const uploader = multer({ storage });
 
 export default uploader;
